3f45ccb9e9939bf1b388f7ca58fbca612b3d8053
russich555 <Kutsokondima@yandex.ru>
2023-10-28 21:59:56 +0300

    to pypi

--- src/moex_api/moex.py
+++ src/moex_api/moex.py
@@ -1,7 +1,4 @@
 import asyncio
-from datetime import datetime, timedelta
-from urllib import parse
-import math
 
 from aiohttp import ClientSession
 
@@ -46,51 +43,6 @@ def get_all_rus_shares():
         print('Repeating request to MOEX API')
         j = flatten(query(f'/engines/stock/markets/shares/boards/TQBR/securities'), 'securities')
     return [row['SECID'] for row in j]
-
-
-def get_index_composition(index: str):
-    return pd.DataFrame(
-        flatten(query(f'/statistics/engines/stock/markets/index/analytics/{index}.json?limit=100'), 'analytics'))[
-        'ticker']
-
-
-def query(method: str, **kwargs):
-    try:
-        url = f'https://iss.moex.com/iss{method}.json'
-        if kwargs:
-            url += "?" + parse.urlencode(kwargs)
-
-        r = requests.get(url, timeout=10)
-        r.encoding = 'utf-8'
-        j = r.json()
-        return j
-
-    except Exception as e:
-        raise e
-
-
-def flatten(j: dict, blockname: str):
-    """
-    Собираю двумерный массив (словарь)
-    :param j:
-    :param blockname:
-    :return:
-    """
-    return [{k: r[i] for i, k in enumerate(j[blockname]['columns'])} for r in j[blockname]['data']]
-
-
-def format_date_list(tradedates: list):
-    return [(datetime.strptime(date, '%Y-%m-%d')).strftime('%d.%m.%y') for date in tradedates]
-
-
-def change_axis_range(l: list, max_len: int):
-    l = list(l)
-    n = 1
-    while len(l) / n > max_len:
-        n += 1
-    x = [l[i] for i in range(0, len(l), n)]
-    if l[-1] != x[-1]: x.append(l[-1])
-    return x
 
 
 async def main():
